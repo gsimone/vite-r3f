591fc01ed1220759033bbeb5fe897a7c998ba940
Gianmarco Simone <gsimone@hey.com>
2021-04-23 19:42:07 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import * as React from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function Scene() {
+
+  return (
+    <mesh>
+      <octahedronGeometry />
+      <meshNormalMaterial />
+    </mesh>
+  )
+
+}
+
+function undepth(obj: any) {
+  obj.material.depthWrite = false
+}
 
 function App() {
   return (
     <div id="canvas">
-      <Canvas>
-        <mesh>
-          <octahedronGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      <Canvas camera={{ position: [5, 5, 5]}}>
+        <color attach="background" args={['#17141F']} />
+        
+        <group>
+          <gridHelper ref={undepth}  renderOrder={9000} args={[1000, 1000, '#17141F', '#060606']} />
+          <gridHelper ref={undepth}  renderOrder={9001} color="red" args={[100, 100, '#fff', '#17141F']} scale={10} />
+          <axesHelper ref={undepth} renderOrder={9002} scale={20} />
+        </group>
+        
+        <Scene />
+        <OrbitControls />
       </Canvas>
     </div>
   )
